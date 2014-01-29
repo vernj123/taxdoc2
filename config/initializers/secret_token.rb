94bfca38aca261d3ad371256272106371f8b6d1c
vernj123 <vjacques01@gmail.com>
2014-01-29 14:55:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taxdoc2::Application.config.secret_key_base = '5f8f155d711287d8c721acbdfb4fba3dde48642821f0d0a80d3cda162a2fd6d13dfd1bd95d63cfd8a5501e0fb03902fddcc1d7d1640cce71bb9db47bb08e4b7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Taxdoc2::Application.config.session_store :cookie_store, key: '_taxdoc2_session'
